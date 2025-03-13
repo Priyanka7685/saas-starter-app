c88a5326e17ba775a064a16fe998c5f2778426ff
Priyanka7685 <priyankathakur7685@gmail.com>
2025-03-13 22:02:06 +0530

    add all files

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,56 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import {clerkMiddleware, clerkClient, createRouteMatcher, getAuth } from '@clerk/nextjs/server'
+import { NextRequest, NextResponse } from 'next/server';
 
-export default clerkMiddleware()
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/api/webhook/register",
+  "/sign-up",
+  "/sign-in"
+]);
+
+export default clerkMiddleware(async (auth,req) => {
+  const { userId } = getAuth(req);
+
+  if(!isPublicRoute(req) && !userId) {
+    // If the route is protected and the user is not authenticated, redirect to sign-in
+    return NextResponse.redirect(new URL('/sign-in', req.url));
+  }
+
+  if(userId) {
+     try {
+      const user = (await clerkClient()).users.getUser(userId);
+      const role = (await user).publicMetadata.role as string | undefined
+ 
+     //  admin role redirection
+     if(role === 'admin' && req.nextUrl.pathname === '/dashboard') {
+       return NextResponse.redirect(new URL('/admin.dashboard', req.url))
+     }
+ 
+     // prevent non admin user to access the admin routes
+     if( role !== 'admin' && req.nextUrl.pathname.startsWith('/admin')) {
+       return NextResponse.redirect(new URL("/dashboard", req.url))
+   }
+ 
+   // 
+   if(isPublicRoute(req)) {
+     return NextResponse.redirect(
+       new URL(
+         role === "admin" ? "/admin/dashboard" : "/dashboard",
+         req.url
+       )
+     )
+   }
+  } catch (error) {
+      console.error(error);
+      return NextResponse.redirect(new URL("/error", req.url))
+      
+     }
+}
+
+  // Proceed without authentication for public routes
+  return NextResponse.next();
+  
+})
 
 export const config = {
   matcher: [
